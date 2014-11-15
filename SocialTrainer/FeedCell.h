0a5e260d7a3fbc62c3253449340dfbbd48af1f32
Adam Szeptycki <adamszeptycki@gmail.com>
2014-11-15 13:53:41 +0100

    adds stuff for animated transition

---
+++ SocialTrainer/FeedCell.h
@@ -0,0 +1,16 @@
+//
+//  FeedCell.h
+//  SocialTrainer
+//
+//  Created by Adam Szeptycki on 15/11/14.
+//  Copyright (c) 2014 MediaHackDay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedCell : UITableViewCell
+
+- (UIView*)getVideoView;
+
+@end
+
---
+++ SocialTrainer/PCTransitionFromFirstToSecond.h
@@ -0,0 +1,13 @@
+//
+//  PCTransitionFromFirstToSecond.h
+//  SocialTrainer
+//
+//  Created by Adam Szeptycki on 15/11/14.
+//  Copyright (c) 2014 MediaHackDay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@interface PCTransitionFromFirstToSecond : NSObject <UIViewControllerAnimatedTransitioning>
+
+@end
