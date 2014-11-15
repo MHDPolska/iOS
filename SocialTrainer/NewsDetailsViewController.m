68ece8ec2b892128a6a41970c2bdc7103e32bbcf
Paweł Nużka <pawelqus@gmail.com>
2014-11-15 13:54:05 +0100

    added news details storyboard

---
+++ SocialTrainer/NewsDetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  NewsDetailsViewController.m
+//  SocialTrainer
+//
+//  Created by Paweł Nużka on 15/11/14.
+//  Copyright (c) 2014 MediaHackDay. All rights reserved.
+//
+
+#import "NewsDetailsViewController.h"
+
+@interface NewsDetailsViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@end
+
+@implementation NewsDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+
+@end
