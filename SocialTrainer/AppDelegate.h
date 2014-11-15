d700d63f8a91cc26c6c92b393ec61c3b7d869425
Paweł Nużka <pawelqus@gmail.com>
2014-11-15 10:48:37 +0100

    Initial Commit

---
+++ SocialTrainer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocialTrainer
+//
+//  Created by Paweł Nużka on 15/11/14.
+//  Copyright (c) 2014 MediaHackDay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocialTrainer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocialTrainer
+//
+//  Created by Paweł Nużka on 15/11/14.
+//  Copyright (c) 2014 MediaHackDay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
